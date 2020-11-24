07053c5460a579d4b74eeb1f95b26f32e549b91c
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-11-24 00:17:46 +0000

    sign in form

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./navbar.css"
 
-export default function Navbar (props) {
+export default function Navbar(props) {
   return (
-    <div class="topnav">
-  <a class="active" href="#home">Study<b>Buddy</b></a>
-  <a href="#logged">Logged as </a>
-  <a href="#logged">Logged as </a>
-  <a href="#dm">DM</a>
-  <a href="#logout">Logout</a>
-</div>
+    <div className="topnav">
+      <a className="active" href="#home">Study<b>Buddy</b></a>
+      <a href="#logged">Logged as </a>
+      <a href="#logged">Logged as </a>
+      <a href="#dm">DM</a>
+      <a href="#logout">Logout</a>
+      <form>
+        <input
+          type="text"
+        />
+        <input
+          type="text"
+        />
+        <button type="submit" class="btn btn-primary">Sign in</button>
+      </form>
+        </div>
   )
 }
