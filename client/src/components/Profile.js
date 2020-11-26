e7def669dd8adf08465e58defd457d65aedeb5d9
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-11-26 00:30:29 +0000

    hook form

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,68 +1,61 @@
 import React from "react";
-// find the html code i used in https://bootsnipp.com/snippets/5Moza
-
 import "./profile.css"
 
+// find the html code i used in https://bootsnipp.com/snippets/5Moza
 export default function Profile(props) {
   return (
-    <div>
-      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
-      {/*---- Include the above in your HEAD tag --------*/}
-      <title>Bootstrap Example</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-      <hr />
-      <div className="container bootstrap snippet">
-        <div className="row">
+    <div id="profile_container">
+      <div className="container bootstrap snippet" id="container_profile">
+        <div className="container" id="name_profile">
           <div className="col-sm-10"><h1>Affaf Rai</h1></div>
           <h3>Rating 8.6</h3>
         </div>
-        <div className="row">
+        </div>
+        <div className="container" id="profile_element">
           <div className="col-sm-3">{/*left col*/}
             <div className="text-center">
-              <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" className="avatar img-circle img-thumbnail" alt="avatar" />
+              <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" className="avatar img-circle img-thumbnail" id="avatar"alt="avatar" />
               
             </div><br />
           </div>{/*/col-3*/}
-          <div className="col-sm-9" style={{padding: "50px"}}>
+          <div className="col-sm-9" style={{padding: "50px"}} id="profile_data">
                 <hr />
-                  <div className="form-group">
+                  <div className="form-group profile_data" id="profile_email" >
                       <label><h4>Email:</h4></label>
                       <label><h4>Affaf-rai@hotmail.com</h4></label>
                   </div>
-                  <div className="form-group">
+                  <div className="form-group" id="profile_location">
                       <label><h4>Location:</h4></label>
                       <label><h4>Toronto</h4></label>
                   </div>
-                  <div className="form-group">
+                  <div className="form-group" id="profile_gender">
                     <label><h4>Gender:</h4></label>
                     <label><h4>female</h4></label>
                   </div>
-                  <div className="form-group">
+                  <div className="form-group" id="profile_class">
                       <label><h4>You are a:</h4></label>
                       <label><h4>Student - Mentor</h4></label>
                   </div>
-                  <div className="form-group">
+                  <div className="form-group" id="profile_occupation">
                       <label><h4>Ocupation:</h4></label>
                       <label><h4>Web Developer</h4></label>
                   </div>
-                  <div className="form-group">
+                  <div className="form-group" id="profile_subjects">
                       <label><h4>preferred Subjects:</h4></label>
-                      <label><h4>Web Development - App Development</h4></label>
+                      <label><h4>Web Development, App Development</h4></label>
                   </div>
-                  <div className="form-group">
+                  <div className="form-group" id="profile_description">
                       <label><h4>Description:</h4></label>
                       <label><h4>jfhgfhg cgfhmg , ,g,jg, gufdtrdjytf uf ,g</h4></label>
                   </div>
                   <div className="form-group">
                       <br />
-                      <button type="submit" class="btn btn-primary" id="#edit_btn">Edit Profile</button>
+                      <button type="submit" className="btn btn-primary edit_btn" id="btn_profile">Edit Profile</button>
                   </div>
                 <hr />
            </div>   
         </div>{/*/col-9*/}
-      </div>{/*/row*/}
-    </div>
+      </div>
+    
   );
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,42 +1,63 @@
 import "./register.css"
-import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 export default function Register(props) {
+  
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit= (event) => {
+    console.log("heyheyhey");
+    event.preventDefault();
+  }
+  
+  axios({
+    method: 'POST',
+    url: '/api/users'
+  })
+  
   return (
     <div>
       <div class="container">
         <div class="jumbotron_register">
           <div class="register_container">
-          <form>
+          <form onSubmit={handleSubmit}>
             <h1>Be a <i>Buddy!</i></h1>
             <p>Full name:</p>
             <input
               type='text'
+              name="fullName"
+              ref={register({required: true})}
             />
+            
+            
             <p>Email:</p>
             <input
               type='text'
+              name="email"
+              ref={register({required: true})}
             />
             <p>Password:</p>
             <input
               type='text'
+              name="password"
+              ref={register({required: true})}
             />
             <p>Confirm password:</p>
             <input
               type='text'
+              name="confirmPassword"
+              ref={register({required: true})}
             />
-          </form>
-          <Link to="/profile/edit">
+            <p></p>
 
-          <button type="submit" class="btn btn-primary">Register!</button>
-          </Link>
+            <button type="submit" class="btn btn-primary">Register!</button>
+          </form>
+          
+
+          
+          
           </div>
         </div>
         </div>
