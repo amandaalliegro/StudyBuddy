3b7714a5e45d4e2a1978b7f7f298f01cb163abfa
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-11-27 21:16:31 -0700

    added websocket to server

--- express_backend/chat.js
+++ express_backend/chat.js
@@ -1,8 +1,21 @@
-module.exports = (io) => {
-  // socket.io
-  io.on('connection', (socket) => {
-    console.log('Client Connected!');
-    console.log('sending a message to client');
-    //socket.emit('receiveMessage', 'Hello from server');
+// This is the file to set up the web socket event listners 
+// handling the client connection.... io.on ,,,, conects client (socket)
+// each socket is a client
+
+module.exports = (wss) => {
+ 
+  wss.on('connection', (socket) => {
+    console.log('Client Connected yo!');
+    socket.send(JSON.stringify({msg: "message from server"}))
+    socket.on("message", (data) => {
+      const message = JSON.parse(data)
+      console.log(message)
+
+    })
+    
+    socket.on('close', data => {
+      console.log("client disconnected")
+    });
   });
+
 };
