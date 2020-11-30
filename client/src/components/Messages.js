c56c72a25b9be8cfc53ed0ac55cd9e943993e879
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-11-30 17:52:12 +0000

    navbar

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,6 +1,7 @@
-
 import React, { useState } from "react";
 import './messages.css'
+
+
 export default function Messages(props) {
   const [text, setText] = useState('')
   const sendMessage = (event) => {
@@ -14,13 +15,16 @@ export default function Messages(props) {
 
   }
   const messageList = props.messages.map((message) => <li key= {message.id} >name: {message.user} message: {message.message}</li>)
+  
   return (
+    <div>
     <div id="chat_box">
         <title>chat</title>
   <ul id="messages">{messageList}</ul>
         <form action>
           <input id="m" autoComplete="off" onChange= {(event) => setText(event.target.value)} /> <button onClick= {sendMessage}>Send</button>
         </form>
+      </div>
       </div>
   );
 };
