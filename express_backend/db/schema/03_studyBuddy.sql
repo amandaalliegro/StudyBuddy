4dbdf63a7b198314e4ba7597da263a065c761286
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-11-30 20:16:02 -0700

    db re-org

--- express_backend/db/schema/03_studyBuddy.sql
+++ express_backend/db/schema/03_studyBuddy.sql
@@ -1,16 +1,13 @@
+
+-- C R E A T E   T A B L E S 
+do $$
+BEGIN
+RAISE NOTICE 'CREATING TABLES';
+END;
+$$;
+
+-- CREATE TABLE FOR USERS
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS interests CASCADE;
-DROP TABLE IF EXISTS users_interests CASCADE;
-DROP TABLE IF EXISTS groups_of_study CASCADE;
-DROP TABLE IF EXISTS room_chat CASCADE;
-DROP TABLE IF EXISTS chat CASCADE;
-DROP TABLE IF EXISTS user_groups CASCADE;
-DROP TABLE IF EXISTS public_board CASCADE;
-
-
--- add community table 
-
-
 CREATE TABLE users(
     id SERIAL PRIMARY KEY NOT NULL,
     full_name VARCHAR(255) NOT NULL,
@@ -24,42 +21,102 @@ CREATE TABLE users(
     silent_buddy boolean DEFAULT FALSE,
     description VARCHAR(255),
     subject VARCHAR(255)
-
-
 );
+
+-- CREATE TABLE FOR MANAGING INTERESTS ('Programming')
+
+DROP TABLE IF EXISTS interests CASCADE;
 CREATE TABLE interests(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255)
 );
+
+-- CREATE TABLE TO MANAGE SPECIFIC USER INTERESTS ('interests(0) => 'Programming')
+
+DROP TABLE IF EXISTS users_interests CASCADE;
 CREATE TABLE users_interests(
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   interest_id INTEGER REFERENCES interests(id) ON DELETE CASCADE
 );
-CREATE TABLE groups_of_study(
+
+-- CREATE TABLE STUDY GROUPS TO GROUP USERS BY INTEREST
+
+DROP TABLE IF EXISTS study_groups CASCADE;
+CREATE TABLE study_groups(
   id SERIAL PRIMARY KEY,
-  interest_id INTEGER REFERENCES interests(id) ON DELETE CASCADE,
   mentor BOOLEAN DEFAULT FALSE,
-  new BOOLEAN DEFAULT FALSE,
-  -- how to set max participants?
-  n_participants INTEGER NOT NULL 
+  new BOOLEAN DEFAULT TRUE,
+  user_count INTEGER DEFAULT 1
 );
--- join table
+
+-- CREATE TABLE USER GROUPS TO GROUP USERS BY INTEREST
+
+DROP TABLE IF EXISTS user_groups CASCADE;
 CREATE TABLE user_groups(
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  group_of_study_id INTEGER REFERENCES groups_of_study(id)
+  study_group_id INTEGER REFERENCES study_groups(id)
 );
--- same as direct message table. then expand to multi user messaging 
 
+-- CREATE TABLE ROOM CHAT TO HOLD MESSAGES AND USER CHATS
+
+DROP TABLE IF EXISTS room_chat CASCADE;
 CREATE TABLE room_chat(
   id SERIAL PRIMARY KEY,
-  new BOOLEAN DEFAULT FALSE,
+  new BOOLEAN DEFAULT TRUE,
   interest_id INTEGER REFERENCES interests(id) ON DELETE CASCADE,
-  group_of_study_id INTEGER REFERENCES groups_of_study(id) ON DELETE CASCADE,
-  messages VARCHAR(255)
+  study_group_id INTEGER REFERENCES study_groups(id) ON DELETE CASCADE
 );
+
+
+-- CREATE TABLE MESSAGES TO HOLD ALL MESSAGES KEYED BY USER AND ROOMCHAT
+
+DROP TABLE IF EXISTS messages CASCADE;
+CREATE TABLE messages(
+  id SERIAL PRIMARY KEY,                          -- MESSAGE ID
+  roomchat_id INTEGER REFERENCES room_chat(id),   -- ROOMCHAT FK
+  user_id INTEGER REFERENCES users(id),           -- USER WHO MADE MSG
+  message TEXT NOT NULL,                          -- TEXT
+  date DATE DEFAULT NOW()                         -- DTG
+);
+
+-- CREATE TABLE FOR PUBLIC CHATS
+DROP TABLE IF EXISTS public_board CASCADE;
 CREATE TABLE public_board(
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id),
-  message VARCHAR(255)
+  message TEXT
 );
 
+-- I N S E R T S
+do $$
+BEGIN
+RAISE NOTICE 'INSERTING VALS';
+END;
+$$;
+
+-- U S E R S
+INSERT INTO users(id, full_name, password, student, mentor, location, language, gender, email, silent_buddy, description, subject)
+VALUES 
+(1, 'Joe Levesque', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', TRUE, FALSE, 'edmonton', 'english', 'male', 'socialconsequences@gmail.com', TRUE, 'i love to study webDevelpment', 'web Development'),
+(2, 'Affaf Rai', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', TRUE, TRUE, 'Toronto', 'english', 'female', 'affaf-rai@hotmail.com', FALSE, 'optimistic about getting my chimistry project done', 'Chemistry'),
+(3, 'Amanda Arnaut', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', TRUE, TRUE, 'Toronto', 'english', 'female', 'amanda.m.arnaut@gmail.com', FALSE, 'looking for people to study with me 3 hours a week', 'science');
+
+-- I N T E R E S T S  
+INSERT INTO interests(id, name) VALUES (1, 'Programming');
+
+-- U S E R   I N T E R E S T S
+INSERT INTO users_interests(user_id, interest_id) VALUES (1, 1);
+
+-- G R O U P    O F    S T U D Y  
+INSERT INTO study_groups(id, user_count) VALUES (1, 1);
+
+-- R O O M   C H A T S
+INSERT INTO room_chat(interest_id, study_group_id) VALUES (1,1);
+
+-- M E S S A G E S
+INSERT INTO messages(roomchat_id, user_id, message)
+VALUES
+-- Preseeded messages
+  (1, 1, 'This is a test'),
+  (1, 1, 'This is a second message'),
+  (1, 2, 'This is a reply from another user');
