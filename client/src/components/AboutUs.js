7d73c4e57ce9027d180d5e0a2eef01235e3f1846
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-12-01 18:51:13 +0000

    navbar by affaf

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -6,9 +6,11 @@ export default function AboutUs() {
     
   
 return (
-  <div>
-    
-    <h2>THIS IS ABOUT US</h2>
+  <div id='about-bd'>
+    <h1>About Us</h1>
+    <p>Study buddy is a community where you can find mates to study together. You and your buddies will be able to find each other by doing a search using names or your interests in common. You can start a conversation and decide to go forward with the study and even add other people on the chat and have a group.
+Set yourself as a student, mentor, or even silent student and enjoy our community by making your contributions on the chats!
+Be a buddy, help us to build well-structured knowledge!</p>
     </div>
   
 )
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,25 +1,29 @@
 import { Container, Form, Alert } from 'react-bootstrap'
 import axios from 'axios';
 import React, { useState } from 'react'
+
+import "./login.css"
+
 import "./navbar.css"
 import { useHistory } from 'react-router-dom';
+
+
 export default function Login(props) {
-  // 
   let history = useHistory();
   const [msg, setMsg] = useState(null)
   function handleSubmit(event) {
     event.preventDefault()
-    // login credentials 
     const newUser = {
       email: event.target[0].value,
       password: event.target[1].value
     }
-    // sets the local storage to the users id stored in db. 
     axios.post('/login', newUser).then((res) => {
       if (res.status === 200) {
+        
           localStorage.setItem("id", res.data.id)
-          // gets info from the axios and can pass it to app.js
+          
           props.setUser(res.data) 
+        
         // set state and useHistory
       }
     }).catch((err) => {
@@ -28,20 +32,21 @@ export default function Login(props) {
     })
   }
   return (
-    <Container>
-      <div className='login_form'>
+
+    <Container id='login_container'>
+      <div id='login_form'>
         {msg && <Alert variant='danger'>{msg}</Alert>}
-        <div className='container-login' id='container-lgn'>
-          <Form id='login-frm' onSubmit={handleSubmit}>
-            <Form.Group controlId='formEmail'>
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type='email' placeholder='Enter email' />
+        <div id='login_form'>
+          <Form id='login_form' onSubmit={handleSubmit}>
+            <Form.Group  id='login_email' controlId='formBasicEmail'>
+              <Form.Label id='login_email1' >Email:</Form.Label>
+              <Form.Control id='login_input'type='email' placeholder='Enter email' />
             </Form.Group>
-            <Form.Group controlId='formPassword'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type='password' placeholder='Password' />
+            <Form.Group id='login_password' controlId='formBasicPassword'>
+              <Form.Label id='login_password1' >Password:</Form.Label>
+              <Form.Control id='login_input'type='password' placeholder='Password' />
             </Form.Group>
-            <button variant='primary' type='submit' onClick={() => history.push('/home/:id')}>Login</button>
+            <button id='login_button' variant='primary' type='submit' onClick={() => history.push('/home/:id')}>Login</button>
           </Form>
         </div>
       </div>
